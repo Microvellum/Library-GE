47e87503d8a3d45eae8a8de47f8c233c892b736d
MattHonnies <dev.matthonnies@gmail.com>
2016-12-27 11:20:27 -0800

    Added Built in appliance operator

--- appliance_operators.py
+++ appliance_operators.py
@@ -144,6 +144,74 @@ class OPERATOR_Place_Appliance_Object(bpy.types.Operator):
             return {'FINISHED'}    
         
         return self.faucet_drop(context,event)
+    
+class OPERATOR_Place_Built_In_Appliance(bpy.types.Operator):
+    bl_idname = appliance_properties.LIBRARY_NAME_SPACE + ".place_built_in_appliance"
+    bl_label = "Place Built In Appliance"
+    bl_description = "This allows you to place a built in appliance. The assembly that you select will automatically be replaced."
+    bl_options = {'UNDO'}
+    
+    #READONLY
+    object_name = bpy.props.StringProperty(name="Object Name")
+    
+    assembly = None
 
+    def invoke(self, context, event):
+        bp = bpy.data.objects[self.object_name]
+        self.assembly = fd_types.Assembly(bp)
+        utils.set_wireframe(self.assembly.obj_bp,True)
+        context.window.cursor_set('PAINT_BRUSH')
+        context.scene.update() # THE SCENE MUST BE UPDATED FOR RAY CAST TO WORK
+        context.window_manager.modal_handler_add(self)
+        return {'RUNNING_MODAL'}
+
+    def cancel_drop(self,context,event):
+        if self.assembly:
+            utils.delete_object_and_children(self.assembly.obj_bp)
+        bpy.context.window.cursor_set('DEFAULT')
+        return {'FINISHED'}
+
+    def appliance_drop(self,context,event):
+        selected_point, selected_obj = utils.get_selection_point(context,event)
+        bpy.ops.object.select_all(action='DESELECT')
+        sel_assembly_bp = utils.get_assembly_bp(selected_obj)
+        
+        if sel_assembly_bp:
+            selected_assembly = fd_types.Assembly(sel_assembly_bp)
+            self.assembly.obj_bp.parent = selected_assembly.obj_bp.parent
+            self.assembly.obj_bp.location = selected_assembly.obj_bp.location
+            self.assembly.obj_z.location.z = selected_assembly.obj_z.location.z
+            self.assembly.obj_y.location.y = selected_assembly.obj_y.location.y
+            self.assembly.obj_x.location.x = selected_assembly.obj_x.location.x
+        else:
+            self.assembly.obj_bp.parent = None
+            self.assembly.obj_bp.location = selected_point
+            
+        if event.type == 'LEFTMOUSE' and event.value == 'PRESS':
+            if sel_assembly_bp:
+                selected_assembly = fd_types.Assembly(sel_assembly_bp)
+                selected_assembly.replace(self.assembly)
+            utils.set_wireframe(self.assembly.obj_bp,False)
+            bpy.context.window.cursor_set('DEFAULT')
+            bpy.ops.object.select_all(action='DESELECT')
+            context.scene.objects.active = self.assembly.obj_bp
+            self.assembly.obj_bp.select = True
+            return {'FINISHED'}
+        
+        return {'RUNNING_MODAL'}
+    
+    def modal(self, context, event):
+        context.area.tag_redraw()
+        
+        if event.type in {'MIDDLEMOUSE', 'WHEELUPMOUSE', 'WHEELDOWNMOUSE'}:
+            return {'PASS_THROUGH'}
+        
+        if event.type in {'ESC'}:
+            self.cancel_drop(context,event)
+            return {'FINISHED'}
+        
+        return self.appliance_drop(context,event)
+    
+bpy.utils.register_class(OPERATOR_Place_Built_In_Appliance)
 bpy.utils.register_class(OPERATOR_Place_Countertop_Appliance)
 bpy.utils.register_class(OPERATOR_Place_Appliance_Object)
